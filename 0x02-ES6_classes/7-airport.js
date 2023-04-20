a79d3d27badba70a5dce7668dd74fd8946f0b9c6
Bairoking <gs8004rtj5jj5j@gmail.com>
2023-04-20 05:15:07 -0700

    Task 7

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,11 @@
+/* Sanna Bah */
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+}
+
+Airport.prototype.toString = function airportToString() {
+  return `[object ${this._code}]`;
+};
